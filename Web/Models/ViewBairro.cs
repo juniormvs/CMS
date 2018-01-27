c371ccc9dd999ef85b809f26030e5cced5efc66f
Érico Francisco Soares de Souza <ericoskywalker@hotmail.com>
2018-01-27 18:52:08 +0000

    Deleted Web

--- Web/Models/ViewBairro.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Web.Models
-{
-    public class ViewBairro
-    {
-        public int Id { get; set; }
-        public string Bairro { get; set; }
-        public string Url { get; set; }
-    }
-}
